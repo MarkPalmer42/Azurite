26d25109ed008878b11c67fa4a6abf561a4c1d30
Mark Palmer <mark.palmer4242@gmail.com>
2019-07-27 13:32:47 +0200

    Created an XML validator.

---
+++ LexicalParser/Parser.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace Azurite.LexicalParser
+{
+    public class Parser
+    {
+
+        public Parser(string xmlPath, string xsdPath)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+            settings.Schemas.Add(null, xsdPath);
+            
+            XmlReader reader = XmlReader.Create(xmlPath, settings);
+            
+            while (reader.Read())
+            {
+                
+            }
+        }
+        
+        private void ValidationCallBack(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Error)
+            {
+                throw new Exception("XML exception occured: " + args.Message);
+            }
+            else
+            {
+                Console.WriteLine("Warning: " + args.Message);
+            }
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Azurite.LexicalParser;
+using System;
 
 namespace Azurite
 {
@@ -10,6 +8,16 @@ namespace Azurite
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Parser parser = new Parser("../../lexical_parser_rules.xml", "../../lexical_parser.xsd");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occured: " + e.Message);
+            }
+            
+            Console.Read();
         }
     }
 }
